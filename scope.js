e191218b5f96f5edd4940ac51e6f192a98aab093
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-08 21:03:11 -1000

    Outer and inner functions

--- scope.js
+++ scope.js
@@ -25,7 +25,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
   return innerFunction();
 }
